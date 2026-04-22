f9020428f44f8d04fa6dc47abeb01ab258a77b7e
Guilherme Fuentes <guifuentes8@gmail.com>
2026-04-22 17:12:40 -0300

    save sun

--- hosts/sun/selfhost-services/ollama.nix
+++ hosts/sun/selfhost-services/ollama.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   # configuration.nix
@@ -6,17 +6,20 @@
     enable = true;
     host = "0.0.0.0"; # expõe na rede local (para acessar de outros PCs)
     port = 11434;
+    package = pkgs.ollama-cpu;
 
     # pré-baixa o modelo na ativação do sistema
     loadModels = [
       "deepseek-coder-v2:16b"
+      "qwen2.5-coder:0.5b"
     ];
 
     environmentVariables = {
-      OLLAMA_NUM_THREADS = "48"; # 48 threads - 4 pro sistema
-      OLLAMA_MAX_LOADED_MODELS = "1";
-      OLLAMA_KEEP_ALIVE = "60m";
+      OLLAMA_NUM_THREADS = "32"; # 48 threads - 4 pro sistema
+      OLLAMA_MAX_LOADED_MODELS = "2";
+      OLLAMA_KEEP_ALIVE = "10m";
       OLLAMA_FLASH_ATTENTION = "1"; # reduz uso de memória
     };
   };
+
 }
