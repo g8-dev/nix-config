2d6bd492ada0567cac6c7a7a039eb462697822b6
Guilherme Fuentes <guifuentes8@gmail.com>
2026-05-03 14:33:39 -0300

    save sun

--- hosts/sun/selfhost-services/ollama.nix
+++ hosts/sun/selfhost-services/ollama.nix
@@ -8,14 +8,13 @@
     package = pkgs.ollama-cpu;
     loadModels = [
       "deepseek-coder-v2:16b"
-      "llama3.2:3b"
     ];
     syncModels = true;
     environmentVariables = {
       OMP_NUM_THREADS = "24";
-      OLLAMA_MAX_LOADED_MODELS = "2";
+      OLLAMA_MAX_LOADED_MODELS = "1";
       OLLAMA_KEEP_ALIVE = "-1";
-      OLLAMA_CONTEXT_LENGTH = "4096";
+      OLLAMA_CONTEXT_LENGTH = "8192";
       OLLAMA_BATCH_SIZE = "512";
       OLLAMA_NUM_PARALLEL = "1";
       OLLAMA_FLASH_ATTENTION = "1";
