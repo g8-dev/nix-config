fcf2b77aca0157841842396f78a4bc062a7e7e5b
Guilherme Fuentes <guifuentes8@gmail.com>
2026-08-05 12:02:32 -0300

    save

--- home/planets/01-mercury/default.nix
+++ home/planets/01-mercury/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 
 {
   imports = [
@@ -13,5 +13,19 @@
   targets.genericLinux.enable = true;
 
   stylix.image = ./mercury.png;
+
+  system.stateVersion = lib.mkDefault "24.05";
+
+  # insert Nix-on-Droid config
+
+  home-manager.config =
+    { pkgs, lib, ... }:
+    {
+      # Read home-manager changelog before changing this value
+      home.stateVersion = lib.mkDefault "24.05";
+
+      # insert home-manager config
+      home-manager.config = ../../astronauts/g8.nix;
+    };
 
 }
