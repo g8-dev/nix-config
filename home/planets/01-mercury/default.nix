9e762afce770dca06fc59d1a4c4de7cada081475
Guilherme Fuentes <guifuentes8@gmail.com>
2026-08-05 16:39:31 -0300

    save

--- home/planets/01-mercury/default.nix
+++ home/planets/01-mercury/default.nix
@@ -1,14 +1,10 @@
-{ inputs, ... }:
+{ ... }:
 
 {
   imports = [
     ../../astronauts/g8.nix
     ../../orbits/cli
     ../../orbits/neovim
-
-    inputs.sops-nix.homeModules.sops
-    inputs.stylix.homeModules.stylix
-    inputs.nixvim.homeModules.nixvim
 
   ];
 
